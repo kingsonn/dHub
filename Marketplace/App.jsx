5bad91977e53cda956b00838d97a5cc9058c3f5c
Hanson Braganza <hansonnnnnnn@gmail.com>
2021-12-16 00:08:13 +0530

    Add files via upload

---
+++ Marketplace/App.jsx
@@ -0,0 +1,163 @@
+import { useEffect, useState} from "react";
+import { useMoralis } from "react-moralis";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Redirect,
+} from "react-router-dom";
+import Account from "components/Account";
+import Chains from "components/Chains";
+import NFTBalance from "components/NFTBalance";
+import NFTTokenIds from "components/NFTTokenIds";
+import { Menu, Layout} from "antd";
+import SearchCollections from "components/SearchCollections";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import Text from "antd/lib/typography/Text";
+import NFTMarketTransactions from "components/NFTMarketTransactions";
+const { Header, Footer } = Layout;
+
+const styles = {
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+
+
+  const [inputValue, setInputValue] = useState("explore");
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <Logo />
+          <SearchCollections setInputValue={setInputValue}/>
+          <Menu
+            theme="light"
+            mode="horizontal"
+            style={{
+              display: "flex",
+              fontSize: "17px",
+              fontWeight: "500",
+              marginLeft: "50px",
+              width: "100%",
+            }}
+            defaultSelectedKeys={["nftMarket"]}
+          >
+            <Menu.Item key="nftMarket" onClick={() => setInputValue("explore")} >
+              <NavLink to="/NFTMarketPlace">🛒 Explore Market</NavLink>
+            </Menu.Item>
+            <Menu.Item key="nft">
+              <NavLink to="/nftBalance">🖼 Your Collection</NavLink>
+            </Menu.Item>
+            <Menu.Item key="transactions">
+              <NavLink to="/Transactions">📑 Your Transactions</NavLink>
+            </Menu.Item>
+          </Menu>
+          <div style={styles.headerRight}>
+            <Chains />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+        <div style={styles.content}>
+          <Switch>
+            <Route path="/nftBalance">
+              <NFTBalance />
+            </Route>
+            <Route path="/NFTMarketPlace">
+              <NFTTokenIds inputValue={inputValue} setInputValue={setInputValue}/>
+            </Route>
+            <Route path="/Transactions">
+              <NFTMarketTransactions />
+            </Route>
+          </Switch>
+          <Redirect to="/NFTMarketPlace" />
+        </div>
+      </Router>
+      <Footer style={{ textAlign: "center" }}>
+        <Text style={{ display: "block" }}>
+          ⭐️ Please star this{" "}
+          <a
+            href="https://github.com/ethereum-boilerplate/ethereum-boilerplate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            boilerplate
+          </a>
+          , every star makes us very happy!
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          🙋 You have questions? Ask them on the {""}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://forum.moralis.io/t/ethereum-boilerplate-questions/3951/29"
+          >
+            Moralis forum
+          </a>
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          📖 Read more about{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplat"
+          >
+            Moralis
+          </a>
+        </Text>
+      </Footer>
+    </Layout>
+  );
+};
+
+export const Logo = () => (
+  <div style={{ display: "flex" }}>
+    <div style={{ display: "flex" , marginLeft: "10px"}}>
+    
+    <h1>Marketplace</h1>
+  </div>
+  </div>
+);
+
+export default App;
---
+++ src/App.jsx
@@ -0,0 +1,178 @@
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import Account from "components/Account";
+import Chains from "components/Chains";
+import TokenPrice from "components/TokenPrice";
+import ERC20Balance from "components/ERC20Balance";
+import ERC20Transfers from "components/ERC20Transfers";
+import InchDex from "components/InchDex";
+import NFTBalance from "components/NFTBalance";
+import Wallet from "components/Wallet";
+import { Layout, Tabs } from "antd";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import Contract from "components/Contract/Contract";
+import Ramper from "components/Ramper";
+import MenuItems from "./components/MenuItems";
+import Main from "components/Main"
+const { Header, Footer } = Layout;
+
+const styles = {
+  logo: {
+    fontFamily: "Roboto, sans-serif",
+    color: "#3f9d84",
+    
+  },
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } = useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading) enableWeb3();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <Logo />
+          <MenuItems />
+          <div style={styles.headerRight}>
+            <Chains />
+            <TokenPrice
+              address="0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+              chain="eth"
+              image="https://cloudflare-ipfs.com/ipfs/QmXttGpZrECX5qCyXbBQiqgQNytVGeZW5Anewvh2jc4psg/"
+              size="40px"
+            />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+
+        <div style={styles.content}>
+          <Switch>
+          <Route path="/main">
+                <Main/>
+            </Route>
+            <Route path="/wallet">
+              <Wallet />
+            </Route>
+            <Route path="/1inch">
+              <Tabs defaultActiveKey="1" style={{ alignItems: "center" }}>
+                <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
+                  <InchDex chain="eth" />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
+                  <InchDex chain="bsc" />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={<span>Polygon</span>} key="3">
+                  <InchDex chain="polygon" />
+                </Tabs.TabPane>
+              </Tabs>
+            </Route>
+            <Route path="/erc20balance">
+              <ERC20Balance />
+            </Route>
+            <Route path="/onramp">
+              <Ramper />
+            </Route>
+            <Route path="/erc20transfers">
+              <ERC20Transfers />
+            </Route>
+            <Route path="/nftBalance">
+              <NFTBalance />
+            </Route>
+            <Route path="/contract">
+              <Contract />
+            </Route>
+            <Route path="/">
+              <Redirect to="/wallet" />
+            </Route>
+            <Route path="/ethereum-boilerplate">
+              <Redirect to="/wallet" />
+            </Route>
+            <Route path="/nonauthenticated">
+              <>Please login using the "Authenticate" button</>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      {/* <Footer style={{ textAlign: "center" }}>
+        <Text style={{ display: "block" }}>
+          ⭐️ Please star this{" "}
+          <a
+            href="https://github.com/ethereum-boilerplate/ethereum-boilerplate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            boilerplate
+          </a>
+          , every star makes us very happy!
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          🙋 You have questions? Ask them on the {""}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://forum.moralis.io/t/ethereum-boilerplate-questions/3951/29"
+          >
+            Moralis forum
+          </a>
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          📖 Read more about{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://moralis.io?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplat"
+          >
+            Moralis
+          </a>
+        </Text>
+      </Footer> */}
+    </Layout>
+  );
+};
+
+export const Logo = () => (
+  <div style={{ display: "flex" , marginLeft: "10px"}}>
+    
+    <h1 style={styles.logo}>dHuB</h1>
+  </div>
+);
+
+export default App;
